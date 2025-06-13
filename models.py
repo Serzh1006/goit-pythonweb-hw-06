2f835585bdd304f290a0c89c393688f1823e179b
Sergey Kravchenko <skravchenko223@gmail.com>
2025-06-13 19:53:17 +0200

    create tables and connect to DB. Create alembic file.

---
+++ models.py
@@ -0,0 +1,61 @@
+from sqlalchemy import (
+    Column, Integer, String, Date, ForeignKey, create_engine
+)
+from sqlalchemy.orm import relationship, declarative_base, sessionmaker
+
+DATABASE_URL = "postgresql://postgres:111111@localhost:5432/school_db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+session = sessionmaker(bind=engine)
+connection = session()
+Base = declarative_base()
+
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+
+    students = relationship("Student", back_populates="group")
+
+
+class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String, nullable=False)
+    group_id = Column(Integer, ForeignKey('groups.id'), nullable=False)
+
+    group = relationship("Group", back_populates="students")
+    grades = relationship("Grade", back_populates="student")
+
+
+class Teacher(Base):
+    __tablename__ = 'teachers'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String, nullable=False)
+
+    subjects = relationship("Subject", back_populates="teacher")
+
+
+class Subject(Base):
+    __tablename__ = 'subjects'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    teacher_id = Column(Integer, ForeignKey('teachers.id'), nullable=False)
+
+    teacher = relationship("Teacher", back_populates="subjects")
+    grades = relationship("Grade", back_populates="subject")
+
+
+class Grade(Base):
+    __tablename__ = 'grades'
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey('students.id'), nullable=False)
+    subject_id = Column(Integer, ForeignKey('subjects.id'), nullable=False)
+    grade = Column(Integer, nullable=False)
+    date_received = Column(Date, nullable=False)
+
+    student = relationship("Student", back_populates="grades")
+    subject = relationship("Subject", back_populates="grades")
+
+Base.metadata.create_all(engine)
+connection.close()
