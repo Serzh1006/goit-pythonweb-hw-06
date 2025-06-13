b001c251a7f2c5b49127792a351672def6505419
Sergey Kravchenko <skravchenko223@gmail.com>
2025-06-13 20:17:26 +0200

    formatted by black

--- models.py
+++ models.py
@@ -1,6 +1,4 @@
-from sqlalchemy import (
-    Column, Integer, String, Date, ForeignKey, create_engine
-)
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, declarative_base, sessionmaker
 
 DATABASE_URL = "postgresql://postgres:111111@localhost:5432/school_db"
@@ -10,8 +8,9 @@ session = sessionmaker(bind=engine)
 connection = session()
 Base = declarative_base()
 
+
 class Group(Base):
-    __tablename__ = 'groups'
+    __tablename__ = "groups"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
 
@@ -19,17 +18,17 @@ class Group(Base):
 
 
 class Student(Base):
-    __tablename__ = 'students'
+    __tablename__ = "students"
     id = Column(Integer, primary_key=True)
     full_name = Column(String, nullable=False)
-    group_id = Column(Integer, ForeignKey('groups.id'), nullable=False)
+    group_id = Column(Integer, ForeignKey("groups.id"), nullable=False)
 
     group = relationship("Group", back_populates="students")
     grades = relationship("Grade", back_populates="student")
 
 
 class Teacher(Base):
-    __tablename__ = 'teachers'
+    __tablename__ = "teachers"
     id = Column(Integer, primary_key=True)
     full_name = Column(String, nullable=False)
 
@@ -37,25 +36,26 @@ class Teacher(Base):
 
 
 class Subject(Base):
-    __tablename__ = 'subjects'
+    __tablename__ = "subjects"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    teacher_id = Column(Integer, ForeignKey('teachers.id'), nullable=False)
+    teacher_id = Column(Integer, ForeignKey("teachers.id"), nullable=False)
 
     teacher = relationship("Teacher", back_populates="subjects")
     grades = relationship("Grade", back_populates="subject")
 
 
 class Grade(Base):
-    __tablename__ = 'grades'
+    __tablename__ = "grades"
     id = Column(Integer, primary_key=True)
-    student_id = Column(Integer, ForeignKey('students.id'), nullable=False)
-    subject_id = Column(Integer, ForeignKey('subjects.id'), nullable=False)
+    student_id = Column(Integer, ForeignKey("students.id"), nullable=False)
+    subject_id = Column(Integer, ForeignKey("subjects.id"), nullable=False)
     grade = Column(Integer, nullable=False)
     date_received = Column(Date, nullable=False)
 
     student = relationship("Student", back_populates="grades")
     subject = relationship("Subject", back_populates="grades")
+
 
 Base.metadata.create_all(engine)
 connection.close()
